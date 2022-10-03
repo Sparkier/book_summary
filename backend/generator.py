c969039d910d98ea2d0503937655c37397ec35b7
Daniel Jönsson <daniel.jonsson@liu.se>
2022-10-03 16:20:35 +0200

    Separate text summarization from text-to-image generation

--- backend/generator.py
+++ backend/generator.py
@@ -7,11 +7,9 @@ from pathlib import Path
 import keras_cv
 from PIL import Image
 
-from parse_epub import parse_epub
-
 
 def to_safe_filename(string):
-    return "".join([c for c in string if c.isalpha() or c.isdigit() or c==' ']).rstrip()
+    return "".join([c for c in string if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
 
 
 def parse_json(file_name):
@@ -19,9 +17,8 @@ def parse_json(file_name):
         return json.load(f)
 
 
-def generate_image_from_text(model, output_dir, text, book_title, chapter, paragraph_idx, sentence_idx):
-    print(book_title, chapter, paragraph_idx, sentence_idx)
-    output_dir = Path(output_dir / to_safe_filename(book_title) / str(chapter))
+def generate_image_from_text(model, output_dir, text, text_idx):
+    print(output_dir, text_idx)
     output_dir.mkdir(parents=True, exist_ok=True)
 
     print("Generating image for text:", text)
@@ -31,16 +28,16 @@ def generate_image_from_text(model, output_dir, text, book_title, chapter, parag
     im = Image.fromarray(generated_images[0])
     # Windows does not support too long filenames
     file_name_max_length = 250
-    file_name = output_dir / f"{paragraph_idx}-{sentence_idx}-{to_safe_filename(text[:file_name_max_length])}.png"
+    file_name = output_dir / \
+        f"{text_idx}-{to_safe_filename(text[:file_name_max_length])}.png"
     im.save(file_name)
 
 
-def iterate_level(mode, output_dir, book_title, level, content_in_level):
-    for paragraph_idx, paragraph in enumerate(content_in_level):
-        for sentence_idx, sentence in enumerate(re.split('[.?]', paragraph)):
-            if not to_safe_filename(sentence):
-                continue
-            generate_image_from_text(model, output_dir, sentence, book_title, level, paragraph_idx, sentence_idx)
+def iterate_level(model, output_dir, content_in_level):
+    for idx, text in enumerate(content_in_level):
+        if not to_safe_filename(text):
+            continue
+        generate_image_from_text(model, output_dir, text, idx)
 
 
 if __name__ == '__main__':
@@ -55,11 +52,20 @@ if __name__ == '__main__':
     input_file = Path(args.input_file)
     if input_file.suffix == ".json":
         book_content = parse_json(input_file)
-    elif input_file.suffix == ".epub":
-        book_content = parse_epub(input_file)
     book = book_content["book"]
 
     model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
 
-    for chapter in [0]:
-        iterate_level(model, output_dir, book["title"], chapter, book["chapters"][chapter]["paragraphs"])
+    book_dir = Path(output_dir / to_safe_filename(book["title"]))
+    for chapter in book["chapters"]:
+        ch_num = chapter['num']
+
+        iterate_level(model, Path(book_dir, "chapters", str(ch_num),
+                      "paragraphs"), chapter["paragraphs"])
+        iterate_level(model, Path(book_dir, "chapters", str(ch_num), "paragraph_summaries"),
+                      chapter["paragraph_summaries"])
+        iterate_level(model, Path(book_dir, "chapters", str(ch_num),
+                      "chapter_summary"), chapter["chapter_summary"])
+
+    iterate_level(model, Path(book_dir, "book_summary"),
+                  [book["book_summary"]])
