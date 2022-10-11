93fb6064f95921c0b982e5baf8d17f8f234016a1
Alex <alex@a13x.io>
2022-10-11 14:08:52 +0200

    docs: document the functions and slightly reorganize them

--- backend/book_summarizer.py
+++ backend/book_summarizer.py
@@ -1,24 +1,31 @@
 #! /usr/bin/env python3
 """Summarize chapters and paragraphs of a book."""
-import json
 import argparse
+import json
 from pathlib import Path
-
-from parse_epub import parse_epub
 
 from transformers import pipeline
 
+import util
+
+
 #  Text-to-image model does not support more than 77 tokens (keras_cv.models.StableDiffusion)
 def text_summarization(summarizer, text, min_length=5, max_length=77):
+    """Summarize a given text to a provided length.
+
+    Args:
+        summarizer (Model): the summarization model in use
+        text (string): the text to be summarized
+        min_length (int, optional): the minimal length of the summarization. Defaults to 5.
+        max_length (int, optional): the maximal length of the summarization. Defaults to 77.
+
+    Returns:
+        string: the summarized version of the text
+    """
     text_len = len(text)
     summary = summarizer(text, min_length=min(min_length, text_len),
                          max_length=min(text_len, max_length), truncation=True)
     return summary[0]['summary_text']
-
-
-def parse_json(file_name):
-    with open(file_name, encoding="utf8") as f:
-        return json.load(f)
 
 
 if __name__ == '__main__':
@@ -36,25 +43,25 @@ if __name__ == '__main__':
 
     input_file = Path(args.input_file)
     if input_file.suffix == ".json":
-        book_content = parse_json(input_file)
+        book_content = util.parse_json(input_file)
     elif input_file.suffix == ".epub":
-        book_content = parse_epub(input_file)
+        book_content = util.parse_epub(input_file)
 
     book = book_content["book"]
     summarized_book = book
-    model_id = "sshleifer/distilbart-cnn-12-6"
-    summarizer = pipeline("summarization", model=model_id)
+    MODEL_ID = "sshleifer/distilbart-cnn-12-6"
+    summarization_model = pipeline("summarization", model=MODEL_ID)
     chapter_summaries = []
     for ch_num, chapter in enumerate(book["chapters"]):
         paragraph_summaries = [text_summarization(
-            summarizer, paragraph) for paragraph in chapter["paragraphs"]]
+            summarization_model, paragraph) for paragraph in chapter["paragraphs"]]
         chapter_summary = text_summarization(
-            summarizer, ''.join(paragraph_summaries))
+            summarization_model, ''.join(paragraph_summaries))
         summarized_book["chapters"][ch_num]["paragraph_summaries"] = paragraph_summaries
         summarized_book["chapters"][ch_num]["chapter_summary"] = chapter_summary
         chapter_summaries.append(chapter_summary)
 
-    book_summary = text_summarization(summarizer, ''.join(chapter_summaries))
+    book_summary = text_summarization(summarization_model, ''.join(chapter_summaries))
     book["book_summary"] = book_summary
     with open(Path(output_dir, input_file.stem + '_summarized.json'), 'w', encoding='utf-8') as f:
         json.dump({"book": book}, f)
--- backend/generator.py
+++ backend/generator.py
@@ -1,23 +1,35 @@
 #! /usr/bin/env python3
 """Using Stable Diffusion to generate images from a prompt."""
-import re
-import json
 import argparse
 from pathlib import Path
+
 import keras_cv
 from PIL import Image
 
+import util
+
 
 def to_safe_filename(string):
+    """Convert a string to a safe filename.
+
+    Args:
+        string (string): the string to be converted to a safe filename
+
+    Returns:
+        string: the save filename after conversion
+    """
     return "".join([c for c in string if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
 
 
-def parse_json(file_name):
-    with open(file_name, encoding="utf8") as f:
-        return json.load(f)
-
-
 def generate_image_from_text(model, output_dir, text, text_idx):
+    """Generating an image from a text segment using a diffusion model.
+
+    Args:
+        model (Model): the ml diffusion model used to generate the image
+        output_dir (Path): where to save the generated image
+        text (string): the prompt for which an image is to be generated
+        text_idx (string): the index of the prompt within the level
+    """
     print(output_dir, text_idx)
     output_dir.mkdir(parents=True, exist_ok=True)
 
@@ -25,15 +37,22 @@ def generate_image_from_text(model, output_dir, text, text_idx):
     # Model does not support more than 77 tokens
     max_length = 77
     generated_images = model.text_to_image(text[:max_length], batch_size=1)
-    im = Image.fromarray(generated_images[0])
+    diffusion_image = Image.fromarray(generated_images[0])
     # Windows does not support too long filenames
     file_name_max_length = 250
     file_name = output_dir / \
         f"{text_idx}-{to_safe_filename(text[:file_name_max_length])}.png"
-    im.save(file_name)
+    diffusion_image.save(file_name)
 
 
 def iterate_level(model, output_dir, content_in_level):
+    """Iterate all the content of a level to create images for the texts.
+
+    Args:
+        model (Model): the diffusion model to create images with
+        output_dir (string): where to write the resulting images
+        content_in_level (List[string]): the content of the current level
+    """
     for idx, text in enumerate(content_in_level):
         if not to_safe_filename(text):
             continue
@@ -46,26 +65,26 @@ if __name__ == '__main__':
     parser.add_argument('--output_dir', type=str, help='output dir')
     args = parser.parse_args()
 
-    output_dir = Path(args.output_dir)
-    output_dir.mkdir(parents=True, exist_ok=True)
+    target_dir = Path(args.output_dir)
+    target_dir.mkdir(parents=True, exist_ok=True)
 
     input_file = Path(args.input_file)
     if input_file.suffix == ".json":
-        book_content = parse_json(input_file)
+        book_content = util.parse_json(input_file)
     book = book_content["book"]
 
-    model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+    diffusion_model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
 
-    book_dir = Path(output_dir / to_safe_filename(book["title"]))
+    book_dir = Path(target_dir / to_safe_filename(book["title"]))
     for chapter in book["chapters"]:
         ch_num = chapter['num']
 
-        iterate_level(model, Path(book_dir, "chapters", str(ch_num),
+        iterate_level(diffusion_model, Path(book_dir, "chapters", str(ch_num),
                       "paragraphs"), chapter["paragraphs"])
-        iterate_level(model, Path(book_dir, "chapters", str(ch_num), "paragraph_summaries"),
-                      chapter["paragraph_summaries"])
-        iterate_level(model, Path(book_dir, "chapters", str(ch_num),
+        iterate_level(diffusion_model, Path(book_dir, "chapters", str(ch_num),
+                      "paragraph_summaries"), chapter["paragraph_summaries"])
+        iterate_level(diffusion_model, Path(book_dir, "chapters", str(ch_num),
                       "chapter_summary"), chapter["chapter_summary"])
 
-    iterate_level(model, Path(book_dir, "book_summary"),
+    iterate_level(diffusion_model, Path(book_dir, "book_summary"),
                   [book["book_summary"]])
