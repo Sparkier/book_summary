9841d2e46ff0df259f9ed8981dc875e5f8add584
Alex <alex@a13x.io>
2022-11-09 14:55:04 +0100

    fix: correct types for api.ts returns

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,12 +1,12 @@
 import type { Book } from "./types";
 export async function fetchBooks(): Promise<string[]> {
-  return fetch(`/api/get_books`)
-    .then((response) => response.json())
-    .then((jsonResponse: Object) => jsonResponse["books"] as string[]);
+  const response = await fetch(`/api/get_books`);
+  const jsonResponse = (await response.json()) as { books: string[] };
+  return jsonResponse["books"];
 }
 
 export async function fetchBook(book: string): Promise<Book> {
-  return fetch(`/api/get_book/${book}`)
-    .then((response) => response.json())
-    .then((jsonResponse: Object) => jsonResponse["book"] as Book);
+  const response = await fetch(`/api/get_book/${book}`);
+  const jsonResponse = (await response.json()) as { book: Book };
+  return jsonResponse["book"];
 }
