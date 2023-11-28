648c95ba2b4c4dda41241dff3f36d9834f82e1ce
Michael <michael.gloeckler@uni-ulm.de>
2023-11-28 15:14:37 +0100

    Prettier updates

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -20,8 +20,7 @@
 
 		const formData = new FormData();
 		formData.append('file', file);
- 
-		
+
 		try {
 			const response = await fetch('http://127.0.0.1:5000/api/upload_book', {
 				method: 'POST',
