d817cc8da334e7477dac7af0ee04c91cb8f318f0
Alex Bäuerle <alex@a13x.io>
2024-04-22 08:49:32 -0700

    feat: actually throw an error on load fail and display the message (#29)

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.error}
+	<h1>{$page.error.message}</h1>
+{:else}
+	<div>
+		<h1>Are you sure this is what you are looking for? I get a 404 here 🙈</h1>
+	</div>
+{/if}
