63fc50b30f3f622d53a8e70f393546c2325c6f4d
js357 <jochen.schmidt1@web.de>
2024-04-29 01:58:33 +0200

    Add fallback image

--- frontend/src/lib/elements/BookCard.svelte
+++ frontend/src/lib/elements/BookCard.svelte
@@ -6,7 +6,7 @@
 
 <div
 	class="flex flex-col border w-64 h-64 rounded-lg shadow-2xl"
-	style="background: linear-gradient(to right, rgb(60, 13, 20) 3px, rgba(255, 255, 255, 0.5) 5px, rgba(255, 255, 255, 0.25) 7px, rgba(255, 255, 255, 0.25) 10px, transparent 12px, transparent 16px, rgba(255, 255, 255, 0.25) 17px, transparent 22px), url({imageSrc});"
+	style="background: linear-gradient(to right, rgb(60, 13, 20) 3px, rgba(255, 255, 255, 0.5) 5px, rgba(255, 255, 255, 0.25) 7px, rgba(255, 255, 255, 0.25) 10px, transparent 12px, transparent 16px, rgba(255, 255, 255, 0.25) 17px, transparent 22px), url({imageSrc}), url({'EmptyImage.jpg'})"
 >
 	<div class="flex-1 flex flex-col justify-between p-2">
 		<h3
