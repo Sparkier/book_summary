313e64fe910a8729beeeacb9fc2ad8c5708406cc
Daniel Jönsson <daniel.jonsson@liu.se>
2024-05-13 21:29:09 +0200

    Formating

--- frontend/src/lib/elements/ProgressBar.svelte
+++ frontend/src/lib/elements/ProgressBar.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-    import { twMerge } from 'tailwind-merge';
+	import { twMerge } from 'tailwind-merge';
 	export let progress: number;
-    export let progressError: string | undefined = undefined;
-    export let width: number = 200;
+	export let progressError: string | undefined = undefined;
+	export let width: number = 200;
 
-    export let classNames = '';
+	export let classNames = '';
 </script>
 
-<div class={twMerge("w-full bg-gray-200 rounded-lg", classNames)} style="width:{width}px">
-    <div
-        class="bg-blue-500 text-xs leading-none py-1 text-center text-white rounded-lg"
-        style="width: {Math.floor(progress)}%"
-    >
-    <p class="ml-1">{Math.floor(progress)}%</p>
-        {#if progressError}
-            <p class="text-red-600">{progressError}</p>
-        {/if}
-    </div>
+<div class={twMerge('w-full bg-gray-200 rounded-lg', classNames)} style="width:{width}px">
+	<div
+		class="bg-blue-500 text-xs leading-none py-1 text-center text-white rounded-lg"
+		style="width: {Math.floor(progress)}%"
+	>
+		<p class="ml-1">{Math.floor(progress)}%</p>
+		{#if progressError}
+			<p class="text-red-600">{progressError}</p>
+		{/if}
+	</div>
 </div>
