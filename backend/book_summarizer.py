23e8a58e77ff6a4a2d53ebcb463d6a8b27c5228e
js357 <44328329+js357@users.noreply.github.com>
2024-06-06 13:25:56 +0200

    New summarizer with example (#41)
    * refactor: change to new summarizer
    
    * Add example summary for Alice in Wonderland
    
    * refactor: change to new summarizer
    
    * Add example summary for Alice in Wonderland
    
    * backend: merge fix
    
    * frontend. Warn fix
    
    * Backend: Save summary config and fix compile errors
    
    * Frontend: Format
    
    * Backend: Ignore no-name-in-module warning for semantic_text_splitter
    
    ---------
    
    Co-authored-by: Daniel Jönsson <daniel.jonsson@liu.se>

--- backend/book_summarizer.py
+++ backend/book_summarizer.py
@@ -5,8 +5,13 @@ from typing import Callable
 import argparse
 import json
 from pathlib import Path
-from transformers import AutoTokenizer, pipeline
+from transformers import pipeline
+from tokenizers import Tokenizer
+import torch.cuda
 from tqdm import tqdm
+
+from semantic_text_splitter import TextSplitter  # pylint: disable=no-name-in-module
+
 import util
 
 
@@ -16,31 +21,69 @@ class BookSummarizer:
     summarized content to the specified output directory.
     """
 
-    def __init__(self, model_id="sshleifer/distilbart-cnn-12-6"):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
-        self.summarizer = pipeline("summarization", model=model_id)
-
-    def text_summarization(self, text, min_length=5, max_length=77):
+    def __init__(
+        self, model_id="pszemraj/led-large-book-summary", min_length=32, max_length=77
+    ):
         """
         Summarize a given text to a provided length.
 
         Args:
-            text (string): the text to be summarized
+            model_id (string): Huggingface model id
             min_length (int, optional): the minimal length of the summarization. Defaults to 5.
             max_length (int, optional): the maximal length of the summarization. Defaults to 77.
 
         Returns:
             string: the summarized version of the text
         """
-        tokens = self.tokenizer.tokenize(
-            self.tokenizer.decode(self.tokenizer.encode(text))
+        self.tokenizer = Tokenizer.from_pretrained(model_id)
+        self.summarizer = pipeline(
+            "summarization",
+            model=model_id,
+            device=0 if torch.cuda.is_available() else -1,
+            min_length=min_length,
+            max_length=max_length,
+            no_repeat_ngram_size=3,
+            encoder_no_repeat_ngram_size=3,
+            repetition_penalty=3.5,
+            num_beams=4,
+            early_stopping=True,
         )
-        text_len = len(tokens)
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def semantic_text_split(self, text, max_tokens):
+        """Split text into chunks accoding to https://github.com/benbrandt/text-splitter. (v0.12.3)
+
+        Args:
+            text (string): the text to be chunked
+            max_tokens (int): maximal length of a chunk
+
+        Returns:
+            list: chunks (strings) of text
+        """
+        splitter = TextSplitter.from_huggingface_tokenizer(self.tokenizer, max_tokens)
+        chunks = splitter.chunks(text)
+        return chunks
+
+    def text_summarization(self, text):
+        """
+        Summarize a given text to a provided length.
+
+        Args:
+            text (string): the text to be summarized
+        Returns:
+            string: the summarized version of the text
+        """
+        tokens = self.tokenizer.encode(text)
+
+        num_tokens = len(tokens)
+
         summary = self.summarizer(
             text,
-            min_length=min(min_length, text_len),
-            max_length=min(text_len, max_length),
-            truncation=True,
+            # Avoid warning:
+            # Your max_length is set to X, but your input_length is only Y. "
+            min_length=min(self.min_length, num_tokens),
+            max_length=min(self.max_length, num_tokens),
         )
         return summary[0]["summary_text"]
 
@@ -60,7 +103,7 @@ class BookSummarizer:
             output_dir (str): The path to the output directory where
             the summarized content will be saved. If unspecified,
             the output directory will be the same as the input file's parent directory.
-            progress_callback (Callable[[int, int], None]): Called with number of processed 
+            progress_callback (Callable[[int, int], None]): Called with number of processed
             items and total items as arguments if not None.
 
         Returns:
@@ -69,10 +112,9 @@ class BookSummarizer:
         # pylint: disable=too-many-locals
         output_dir.mkdir(parents=True, exist_ok=True)
 
-
         book_content = util.parse_book(input_file)
 
-        book = book_content["book"]
+        book: dict = book_content["book"]
         summarized_book = book
         chapter_summaries = []
 
@@ -96,27 +138,46 @@ class BookSummarizer:
         increment_progress(0)
 
         for ch_num, chapter in enumerate(book["chapters"]):
-            paragraph_summaries = []
+            translation_table = dict.fromkeys(map(ord, '\n*\xa0\u2009""'), None)
+            # Clean paragraphs
+            clean_paragraphs = [
+                paragraph.translate(translation_table)
+                for paragraph in chapter["paragraphs"]
+            ]
 
-            for paragraph in chapter["paragraphs"]:
-                paragraph_summaries.append(self.text_summarization(paragraph))
-                increment_progress(1)
-
-            chapter_summary = self.text_summarization("".join(paragraph_summaries))
-            increment_progress(1)
-
+            chapter_text: str = "\n".join(clean_paragraphs)
+            chapter_chunks: list = self.semantic_text_split(
+                chapter_text, self.summarizer.tokenizer.model_max_length
+            )
+            chapter_chunk_summaries: list = [
+                self.text_summarization(chunk) for chunk in chapter_chunks
+            ]
             summarized_book["chapters"][ch_num][
                 "paragraph_summaries"
-            ] = paragraph_summaries
+            ] = chapter_chunk_summaries
+
+            chapter_summary: str = "\n".join(chapter_chunk_summaries)
+            increment_progress(1)
+
             summarized_book["chapters"][ch_num]["chapter_summary"] = chapter_summary
             chapter_summaries.append(chapter_summary)
 
-        book_summary = self.text_summarization("".join(chapter_summaries))
+        chapter_summary_chunks: list = self.semantic_text_split(
+            "".join(chapter_summaries), self.summarizer.tokenizer.model_max_length
+        )
+        chapter_chunk_summaries: list = [
+            self.text_summarization(chunk) for chunk in chapter_summary_chunks
+        ]
+        book_summary = self.text_summarization("\n".join(chapter_chunk_summaries))
         increment_progress(1)
 
         book["book_summary"] = book_summary
+
         with open(Path(output_dir, "summarized.json"), "w", encoding="utf-8") as f:
             json.dump({"book": book}, f)
+        self.summarizer.model.config.to_json_file(
+            Path(output_dir, "summarized_config.json")
+        )
 
         return True
 
